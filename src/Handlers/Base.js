e2d63065bc9e0ca68fdccd9cce556e17208e998b
Andrew Sheffield <ajsheff@apple.com>
2018-03-18 17:27:11 -0700

    Updated default URI to dynamically build the URI in environment

--- src/Handlers/Base.js
+++ src/Handlers/Base.js
@@ -1,21 +1,14 @@
 /* @flow */
 import { description, author, license, version } from '../../package.json';
 
-type Request = {
-  server: {
-    info: {
-      uri: string
-    }
-  }
-};
+export default async function(request: Object, h: Object) {
+  const uri: string = process.env.HOST || 'http://localhost:3000';
 
-export default async function(request: Request, h: Object) {
-  console.log(process.env);
   return {
     description,
     author,
     license,
     version,
-    documentation: request.server.info.uri + '/documentation'
+    documentation: uri + '/documentation'
   };
 }
